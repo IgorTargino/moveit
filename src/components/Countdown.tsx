a5042e654f246796b4eedc4af497d23d755f3f55
IgorTargino <igortargino01@gmail.com>
2021-02-25 12:49:43 -0300

    fix Countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -7,7 +7,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function Countdown() {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
